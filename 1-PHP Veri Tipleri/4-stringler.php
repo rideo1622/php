186a9828c9b6747f761a07673c19f25244a6be40
rideo1622 <52043299+rideo1622@users.noreply.github.com>
2023-06-13 09:33:53 +0300

    Update 4-stringler.php

--- 1-PHP Veri Tipleri/4-stringler.php
+++ 1-PHP Veri Tipleri/4-stringler.php
@@ -9,16 +9,16 @@ $yas = 19;
 // $mesaj = "Benim adım " . $ad . " " . $soyad . " ve yaşım " . $yas;
 $mesaj = "Ben {$ad} {$soyad}. Ben {$yas} yaşındayım.";
 
-echo $mesaj . "<br>";
-echo $mesaj[0] . "<br>"; //B harfi çıkar
-echo $mesaj[2] . "<br>"; //n harfi çıkar
+echo $mesaj . "<br>"; // Mesajı ekrana yazdırır
+echo $mesaj[0] . "<br>"; // İlk karakteri (B) ekrana yazdırır
+echo $mesaj[2] . "<br>"; // Üçüncü karakteri (n) ekrana yazdırır
 
-//String fonksiyonları
-echo strlen($mesaj) . "<br>";
-echo str_word_count($mesaj) . "<br>";
-echo strtolower($mesaj) . "<br>";
-echo strtoupper($mesaj) . "<br>";
-echo ucfirst($mesaj) . "<br>";
-echo str_replace(["Rıdvan", "19"], ["A.", "25"], $mesaj) . "<br>";
+// String fonksiyonları
+echo strlen($mesaj) . "<br>"; // Mesajın karakter sayısını ekrana yazdırır
+echo str_word_count($mesaj) . "<br>"; // Mesajdaki kelime sayısını ekrana yazdırır
+echo strtolower($mesaj) . "<br>"; // Mesajı küçük harflere dönüştürüp ekrana yazdırır
+echo strtoupper($mesaj) . "<br>"; // Mesajı büyük harflere dönüştürüp ekrana yazdırır
+echo ucfirst($mesaj) . "<br>"; // Mesajın ilk harfini büyük harf yapar ve ekrana yazdırır
+echo str_replace(["Rıdvan", "19"], ["A.", "25"], $mesaj) . "<br>"; // Mesajdaki "Rıdvan" ve "19" ifadelerini sırasıyla "A." ve "25" ile değiştirip ekrana yazdırır
 
 ?>
